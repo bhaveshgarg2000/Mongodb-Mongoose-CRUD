fb6db96735298603d5d4508c004910c9dedf8262
bhaveshgarg2000 <bhaveshgarg2005@gmail.com>
2022-06-22 16:48:29 +0530

    CRUD OPERATION DONE & ADD ROUTER

--- Server.js
+++ Server.js
@@ -1,32 +1,14 @@
-const express = require('express');
+const express = require("express");
+require("./db/db");
+const Student = require("./models/students");
+const studentRouter = require("./routers/student");
+
 const app = express();
-const Student = require('./models/students');
-
 const port = process.env.PORT || 8080;
-const mongoose = require('./db/db');
-app.use(express.json())
 
-
-app.get('/', (req, res) => {
-    res.send('root route');
-})
-
-app.post('/students', (req, res) => {
-    // res.send('Hello Students');
-    console.log(req.body)
-    const user = new Student(req.body);
-    user.save().then(() => {
-        res.status(201).send(user);
-    }).catch(err => {
-        console.log(err)
-    })
-
-
-})
-
-
-
+app.use(express.json());
+app.use(studentRouter);
 
 app.listen(port, () => {
-    console.log(`Server Running on http://localhost:${port}/ `)
-})
+  console.log(`Server Running on http://localhost:${port}/ `);
+});
---
+++ routers/student.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const router = new express.Router();
+const Student = require('../models/students');
+router.get('/', (req, res) => {
+    res.send('root route');
+})
+
+
+// POST REQUEST API
+router.post('/students', async (req, res) => {
+    try {
+        // res.send("Hello Students")
+        const user = new Student(req.body);
+        const createUser = await user.save();
+        res.status(201).send(createUser);
+        console.log(createUser)
+    } catch (err) {
+        res.status(400).send(err)
+    }
+
+});
+
+// GET REQUEST API
+router.get('/students', async (req, res) => {
+    try {
+        const studentsData = await Student.find()
+        res.send(studentsData);
+    } catch (err) {
+        res.send(err);
+
+    }
+});
+
+
+// GET USER REQUEST USING ID API
+router.get("/students/:id", async (req, res) => {
+    try {
+        const _id = req.params.id;
+        // console.log
+        const studentData = await Student.findById(_id)
+        console.log(studentData)
+        if (!studentData) {
+            return res.status(404).send()
+        } else {
+            return res.status(201).send(studentData)
+        }
+        res.send(studentData);
+    } catch (e) {
+        res.status(500).send(e)
+    }
+});
+
+// DELETE USER USING NAME API
+
+router.delete('/students/:id',async(req,res) => {
+    
+    try{
+        const deleteStudent = await Student.findByIdAndDelete(req.params.id);
+        if(!req.params.id){
+            return res.status(400).send();
+                }
+        res.send(deleteStudent);
+
+    }catch(e){
+        res.status(500).send(e);
+
+    }
+
+});
+
+
+
+
+router.patch('/students/:id',async(req,res)=>{
+    try {
+        const _id = req.params.id;
+        const updateStudent = await Student.findByIdAndUpdate(_id,req.body,{
+            new:true
+        });
+        res.status(200).send(updateStudent);
+        console.log(updateStudent)
+    } catch (err) {
+        res.status(404).send(err);
+        
+    }
+    });
+
+
+module.exports = router;
